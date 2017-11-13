f06b7b1a174ac6b33c116ad20681e3f4918b61ea
Dan Osborne <djosborne10@gmail.com>
2017-11-13 14:19:09 -0800

    Switch to master instead of latest

--- calico/containerized-e2e/semaphore/build.sh
+++ calico/containerized-e2e/semaphore/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 GIT_SHA=$(git rev-parse --short HEAD)
 
-docker build -t gcr.io/unique-caldron-775/k8s-e2e:${GIT_SHA} .
-# TODO: change to latest
+docker build --quiet -t gcr.io/unique-caldron-775/k8s-e2e:${GIT_SHA} .
 docker tag gcr.io/unique-caldron-775/k8s-e2e:${GIT_SHA} gcr.io/unique-caldron-775/k8s-e2e:master
 
--- calico/containerized-e2e/semaphore/push.sh
+++ calico/containerized-e2e/semaphore/push.sh
@@ -3,10 +3,9 @@ GIT_SHA=$(git rev-parse --short HEAD)
 
 if [ "${SEMAPHORE_THREAD_RESULT}" == "passed" ] && [ $BRANCH_NAME == "master" ]; then
   docker push gcr.io/unique-caldron-775/k8s-e2e:${GIT_SHA}
-  docker push gcr.io/unique-caldron-775/k8s-e2e:latest
+  docker push gcr.io/unique-caldron-775/k8s-e2e:master
 
   echo "[INFO] pushed gcr.io/unique-caldron-775/k8s-e2e:${GIT_SHA}"
-  # TODO: change to :latest
   echo "[INFO] pushed gcr.io/unique-caldron-775/k8s-e2e:master"
 else
   echo "[INFO] not pushing."
