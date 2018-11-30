a8757e6752e03de66ca11fed292e50234372c498
Akshay Gawate <akshay.gawate@calsoftinc.com>
2018-11-30 11:40:18 +0000

    Added test cases for CRUD operations on Tier, NetworkPolicy, GlobalNetworkPolicy and GlobalNetworkSet

--- test/e2e/network/tigera_cnx_apiserver.go
+++ test/e2e/network/tigera_cnx_apiserver.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"strings"
-	"time"
 
 	"github.com/ghodss/yaml"
 	. "github.com/onsi/ginkgo"
@@ -14,138 +13,123 @@ import (
 )
 
 var _ = SIGDescribe("[Feature:CNX-APIServer]", func() {
-	var f = framework.NewDefaultFramework("cnx-apiserver")
-	var kubectl *testKubectlCNXAPI
+	var kubectl *calico.Kubectl
+	var jsonData interface{}
 
-	Context("Test CNX API server with 'Tier'", func() {
+	testCNXCRUDAPI := func(resName, resKind, resYamlFile, namespace, patch string, resConfig *yamlConfig) {
+		// Test kubectl can 'Create' the CNX resource 		
+		By(fmt.Sprintf("Checking kubectl command can create a %s %s", resName, resConfig.Name))
+		resManifest := calico.ReadTestFileOrDie(resYamlFile, resConfig)
+		error := kubectl.Create(resManifest, namespace, "")
+		Expect(error).NotTo(HaveOccurred())
+
+		// Test kubectl can 'Get' the CNX resource with different output options
+		By(fmt.Sprintf("Checking kubectl command can get a %s %s", resName, resConfig.Name))
+		output, err := kubectl.Get(resKind, namespace, resConfig.Name, "", "", "", false)
+		Expect(err).NotTo(HaveOccurred())
+		getName := strings.Fields(strings.Split(output, "\n")[1])[0]
+		Expect(getName).To(Equal(resConfig.Name))
+
+		By(fmt.Sprintf("Checking kubectl command can get a %s %s in json output format", resName, resConfig.Name))
+		output, err = kubectl.Get(resKind, namespace, resConfig.Name, "", "json", "", false)
+		Expect(err).NotTo(HaveOccurred())
+		err = json.Unmarshal([]byte(output), &jsonData)
+		getName = jsonData.(map[string]interface{})["metadata"].(map[string]interface{})["name"].(string)
+		Expect(getName).To(Equal(resConfig.Name))
+
+		By(fmt.Sprintf("Checking kubectl command can get a %s %s in yaml output format", resName, resConfig.Name))
+		output, err = kubectl.Get(resKind, namespace, resConfig.Name, "", "yaml", "", false)
+		Expect(err).NotTo(HaveOccurred())
+		err = yaml.Unmarshal([]byte(output), &jsonData)
+		getName = jsonData.(map[string]interface{})["metadata"].(map[string]interface{})["name"].(string)
+		Expect(getName).To(Equal(resConfig.Name))
+
+		// Test kubectl can 'Update' the CNX resource
+		By(fmt.Sprintf("Checking kubectl command can update a %s %s", resName, resConfig.Name))
+		err = kubectl.Patch(resKind, namespace, resConfig.Name, "", patch)
+		Expect(err).NotTo(HaveOccurred())
+
+		// Test kubectl can 'Delete' the CNX resource
+		By(fmt.Sprintf("Checking kubectl command can delete a %s %s", resName, resConfig.Name))
+		err = kubectl.Delete(resKind, namespace, resConfig.Name, "")
+		Expect(err).NotTo(HaveOccurred())
+	}
+
+	Context("Test CNX API server with 'Tier' and 'GlobalNetworkSet'", func() {
 		var (
-			tierConfig *yamlConfig
-			tierName   string
+			tierConfig, gnsConfig *yamlConfig
 		)
 
-		BeforeEach(func() {
-			kubectl = &testKubectlCNXAPI{}
-			tierName = "cnx-test-tier"
-		})
+		// Get a 'kubectl' client
+		kubectl = &calico.Kubectl{}
 
-		AfterEach(func() {
-			kubectl.delete("tier.p", "", tierName)
-		})
-
-		It("creates a Tier using kubectl command", func() {
-			By(fmt.Sprintf("Checking CNX API Server: kubectl command can create a Tier %s", tierName))
+		It("Test the CRUD operations on 'Tier' using kubectl command", func() {
+			// Create a Tier
 			tierConfig = &yamlConfig{
-				Name: createName(tierName),
+				Name: createName("cnx-api-test-tier"),
 			}
-			tier := calico.ReadTestFileOrDie("cnx-tier-1.yaml", tierConfig)
-			err := kubectl.apply(tier, "")
-			Expect(err).NotTo(HaveOccurred())
+			patch := "{\"spec\":{\"order\":90.0}}"
+
+			testCNXCRUDAPI("Tier", "tier.p", "cnx-tier-1.yaml", "", patch, tierConfig)
+		})
+
+		It("Test the CRUD operations on 'GlobalNetworkSet' using kubectl command", func() {
+			// Create a GlobalNetworkSet
+			gnsConfig = &yamlConfig{
+				Name: createName("cnx-api-test-gns"),
+			}
+			patch := "{\"metadata\":{\"labels\":{\"app\":\"frontend\"}}}"
+
+			testCNXCRUDAPI("GlobalNetworkSet", "globalnetworkset.p", "cnx-gns-1.yaml", "", patch, gnsConfig)
 		})
 	})
 
-	Context("Test CNX API server with 'NetworkPolicy'", func() {
+	Context("Test CNX API server with 'NetworkPolicy' and 'GlobalNetworkPolicy'", func() {
 		var (
-			tierConfig, npConfig *yamlConfig
-			testNameSpace        string
-			jsonData             interface{}
+			tierConfig, npConfig, gnpConfig *yamlConfig
+			testNamespace                   string
 		)
 
-		BeforeEach(func() {
-			testNameSpace = f.Namespace.Name
-			kubectl = &testKubectlCNXAPI{}
+		// Get a framework object with a new namespace for testing
+		var f = framework.NewDefaultFramework("cnx-apiserver")
 
-			// Create Tier
+		BeforeEach(func() {
+			testNamespace = f.Namespace.Name
+			kubectl = &calico.Kubectl{}
+
+			// Create a Tier
 			tierConfig = &yamlConfig{
 				Name: createName("cnx-api-test-tier"),
 			}
 			tier := calico.ReadTestFileOrDie("cnx-tier-1.yaml", tierConfig)
-			kubectl.apply(tier, "")
+			kubectl.Create(tier, "", "")
+		})
 
-			// Create a NetworkPolicy within the Tier
+		AfterEach(func() {
+			kubectl.Delete("tier.p", "", tierConfig.Name, "")
+		})
+
+		It("Test the CRUD operations on 'NetworkPolicy' using kubectl command", func() {
+			// Create a NetworkPolicy
 			npConfig = &yamlConfig{
 				Name:     fmt.Sprintf("%s.cnx-api-test-np", tierConfig.Name),
 				TierName: tierConfig.Name,
 			}
-			np := calico.ReadTestFileOrDie("cnx-np-1.yaml", npConfig)
-			kubectl.apply(np, testNameSpace)
+			patch := "{\"spec\":{\"order\":90.0}}"
+
+			testCNXCRUDAPI("NetworkPolicy", "networkpolicy.p", "cnx-np-1.yaml", "", patch, npConfig)
 		})
 
-		AfterEach(func() {
-			kubectl.delete("networkpolicy.p", testNameSpace, npConfig.Name)
-			time.Sleep(3 * time.Second)
-			kubectl.delete("tier.p", "", tierConfig.Name)
-		})
+		It("Test the CRUD operations on 'GlobalNetworkPolicy' using kubectl command", func() {
+			// Create a GlobalNetworkPolicy within the Tier
+			gnpConfig = &yamlConfig{
+				Name:     fmt.Sprintf("%s.cnx-api-test-gnp", tierConfig.Name),
+				TierName: tierConfig.Name,
+			}
+			patch := "{\"spec\":{\"order\":90.0}}"
 
-		It("Get a NetworkPolicy using kubectl command", func() {
-			By(fmt.Sprintf("Checking CNX API Server: kubectl command can get a NetworkPolicy %s", npConfig.Name))
-                        output, err := kubectl.get("networkpolicy.p", testNameSpace, npConfig.Name, "", "")
-                        Expect(err).NotTo(HaveOccurred())
-                        npName := strings.Fields(strings.Split(output, "\n")[1])[0]
-                        Expect(npName).To(Equal(npConfig.Name))
-
-			By(fmt.Sprintf("Checking CNX API Server: kubectl command can get a NetworkPolicy %s in json output format", npConfig.Name))
-			output, err = kubectl.get("networkpolicy.p", testNameSpace, npConfig.Name, "", "json")
-			Expect(err).NotTo(HaveOccurred())
-			err = json.Unmarshal([]byte(output), &jsonData)
-			npName = jsonData.(map[string]interface{})["metadata"].(map[string]interface{})["name"].(string)
-			Expect(npName).To(Equal(npConfig.Name))
-
-			By(fmt.Sprintf("Checking CNX API Server: kubectl command can get a NetworkPolicy %s in yaml output format", npConfig.Name))
-			output, err = kubectl.get("networkpolicy.p", testNameSpace, npConfig.Name, "", "yaml")
-			Expect(err).NotTo(HaveOccurred())
-			err = yaml.Unmarshal([]byte(output), &jsonData)
-			npName = jsonData.(map[string]interface{})["metadata"].(map[string]interface{})["name"].(string)
-			Expect(npName).To(Equal(npConfig.Name))
+			testCNXCRUDAPI("GlobalNetworkPolicy", "globalnetworkpolicy.p", "cnx-gnp-1.yaml", "", patch, gnpConfig)
 		})
 	})
 })
-
-type testKubectlCNXAPI struct {
-}
-
-func (k *testKubectlCNXAPI) create(yaml string, ns string) error {
-	options := []string{"create", "-f", "-"}
-	if ns != "" {
-		options = append(options, fmt.Sprintf("--namespace=%v", ns))
-	}
-	_, err := framework.NewKubectlCommand(options...).WithStdinData(yaml).Exec()
-	return err
-}
-
-func (k *testKubectlCNXAPI) apply(yaml string, ns string) error {
-	options := []string{"apply", "-f", "-"}
-	if ns != "" {
-		options = append(options, fmt.Sprintf("--namespace=%v", ns))
-	}
-	_, err := framework.NewKubectlCommand(options...).WithStdinData(yaml).Exec()
-	return err
-}
-
-func (k *testKubectlCNXAPI) get(kind, ns, name string, label string, outputOption string) (string, error) {
-	options := []string{"get", kind}
-	if outputOption != "" {
-                options = append(options, fmt.Sprintf("-o=%s", outputOption))
-        }
-	if name != "" {
-		options = append(options, name)
-	}
-	if ns != "" {
-		options = append(options, fmt.Sprintf("--namespace=%v", ns))
-	}
-	if label != "" {
-		options = append(options, fmt.Sprintf("-l %s", label))
-	}
-
-	output, err := framework.NewKubectlCommand(options...).Exec()
-	return output, err
-}
-
-func (k *testKubectlCNXAPI) delete(kind, ns, name string) error {
-	options := []string{"delete", kind, name}
-	if ns != "" {
-		options = append(options, fmt.Sprintf("--namespace=%v", ns))
-	}
-	_, err := framework.NewKubectlCommand(options...).Exec()
-	return err
-}
 
