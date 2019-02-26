cf51a0d2b17047911b82220141b909bb3e9df679
Eric Hoffmann <2ffs2nns@gmail.com>
2019-02-26 09:18:23 -0800

    run dockerized go binary from kube-build

--- calico/containerized-e2e/semaphore/build-kubernetes.sh
+++ calico/containerized-e2e/semaphore/build-kubernetes.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 build/run.sh make WHAT=test/e2e/e2e.test
-go build test/list/main.go
-test/list/main test/e2e > calico/containerized-e2e/tests.txt
+docker run -v $(pwd):/k8s $(ls _output/images) bash -c "go build /k8s/test/list/main.go ; ./main /k8s/test/e2e" > calico/containerized-e2e/tests.txt
